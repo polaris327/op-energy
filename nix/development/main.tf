338724f365a6baa31bbe52e57ca012a367f82c26
Dambaev Alexander <ice.redmine@gmail.com>
2021-11-01 23:02:58 +0800

    nix: deploy: development: declare swap file to use as it is useful on the smalest droplet

--- nix/development/main.tf
+++ nix/development/main.tf
@@ -62,6 +62,11 @@ resource "digitalocean_droplet" "droplet_instance" {
                 fileSystems."/" = { device = "/dev/vda1"; options = [ "noatime" "discard"]; };
                 fileSystems."/mnt/bitcoind-signet" = { device = "/dev/disk/by-id/scsi-0DO_Volume_${var.DROPLET_NAME}-bitcoind-signet"; fsType = "xfs"; options = [ "noatime" "discard"]; };
                 fileSystems."/mnt/electrs-signet" = { device = "/dev/disk/by-id/scsi-0DO_Volume_${var.DROPLET_NAME}-electrs-signet"; fsType = "xfs"; options = [ "noatime" "discard"]; };
+                swapDevices = [
+                  { device = "/tmp/swap";
+                    size = 2048;
+                  }
+                ];
               }
           runcmd:
             - curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | PROVIDER=digitalocean NIXOS_IMPORT=./host.nix NIX_CHANNEL=nixos-21.05 bash 2>&1 | tee /tmp/infect.log
