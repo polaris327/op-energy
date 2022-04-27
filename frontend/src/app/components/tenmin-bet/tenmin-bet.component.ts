ad05eb730c818b97fe6d008fd55ade731a9637f9
polaris327 <webstar317@yahoo.com>
2022-04-28 00:51:09 +0800

    oe 10 min bets block view updates

--- frontend/src/app/components/tenmin-bet/tenmin-bet.component.ts
+++ frontend/src/app/components/tenmin-bet/tenmin-bet.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Input } from '@ang
 import { Block } from '../../interfaces/electrs.interface';
 import { StateService } from '../../services/state.service';
 
+interface PastBlock extends Block {
+  mediantimeDiff: number;
+}
+
 @Component({
   selector: 'app-tenmin-bet',
   templateUrl: './tenmin-bet.component.html',
@@ -9,9 +13,20 @@ import { StateService } from '../../services/state.service';
 })
 export class TenminBetComponent implements OnInit, OnDestroy {
   @Input() blockStyle: any;
-  @Input() medianTimeDiff: number;
-  @Input() blockNumber: number;
-  @Input() nlockTime: number;
+  @Input() block: PastBlock;
+  @Input() prevBlock: PastBlock;
+
+  get medianTimeDiff(): number {
+    return this.block.mediantime - this.prevBlock.mediantime;
+  }
+
+  get blockNumber(): number {
+    return this.block.height;
+  }
+
+  get nlockTime(): number {
+    return this.prevBlock.mediantime + 600;
+  }
 
   get isFast() {
     return this.medianTimeDiff < 600;
